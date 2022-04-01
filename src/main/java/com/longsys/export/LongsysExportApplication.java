f7396816e85f7c5b0e28e982b0ee0659c6645904
halosky <626090763@qq.com>
2022-04-01 16:11:34 +0800

    create project

---
+++ src/main/java/com/longsys/export/LongsysExportApplication.java
@@ -0,0 +1,16 @@
+package com.longsys.export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huan.yang
+ */
+@SpringBootApplication
+public class LongsysExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongsysExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longsys/export/LongsysExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longsys.export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongsysExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
