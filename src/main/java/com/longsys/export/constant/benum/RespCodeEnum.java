c0053b0c535e52a5a42176b205b6a9c7299bdc55
halosky <626090763@qq.com>
2022-04-01 18:28:36 +0800

    Add common Resp Code Enum

---
+++ src/main/java/com/longsys/export/constant/benum/RespCodeEnum.java
@@ -0,0 +1,42 @@
+package com.longsys.export.constant.benum;
+
+import lombok.Getter;
+
+/**
+ * @author huan.yang
+ * @date 2022-03-29
+ */
+@Getter
+public enum RespCodeEnum {
+    /**
+     * 成功
+     */
+    SUCC(200,"访问成功"),
+
+    /**
+     * 500
+     */
+    ERROR(500,"系统错误"),
+
+    /**
+     * 403
+     */
+    PERMISSION_E(403,"无权访问");
+
+    /**
+     * 响应码
+     */
+    private final int code;
+
+    /**
+     * 输出信息
+     */
+    private final String msg;
+
+    RespCodeEnum(int code,String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+
+}
